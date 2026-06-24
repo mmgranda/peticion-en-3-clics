c7475d7a23e01105690252db5f4b39388e63694e
mmgranda <mariadelmgranda15@gmail.com>
2026-06-24 21:10:15 +0000

    Desarrollo del bloque 4

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/", (req, res) => {
+  res.send("Petición en 3 Clics funcionando");
+});
+
+app.get("/estado", (req, res) => {
+  res.json({
+    ok: true,
+    mensaje: "Servidor funcionando",
+    proyecto: "Petición en 3 Clics"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
